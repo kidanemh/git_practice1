7ea11977f8f4f6ec484303ee6bc2a8c0d40d4e22
kidane <kidane_mesfin@yahoo.com>
2022-01-10 13:45:13 -0600

    First day Git done about to push to be git

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,16 @@
 public class HelloWorld {
 
+}
+
     // Doing some work here( you can type anything you want )
 
 
     // Doing awesome work2 !
 
+    // each commit must have a message !1
+
+   //  Doing awsome work 3
 
 
 
 
-
-}
